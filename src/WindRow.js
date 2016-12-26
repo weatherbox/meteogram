45afe24df4fca623d0a0f357aa7ef1fce9c39d33
tattii <yuta.tatti@gmail.com>
2016-12-27 03:00:23 +0900

    add wind arrow

--- src/WindRow.js
+++ src/WindRow.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import arrow from './arrow.svg'
 
 export default class WindRow extends Component {
 	render() {
@@ -7,7 +8,15 @@ export default class WindRow extends Component {
 				{this.props.data.map((d, i) => {
 					let speed = d.wind.speed.toFixed(1)
 					if (speed >= 10) speed = Math.round(speed)
-					return (<td key={i}>{speed}</td>)
+
+					let from_dir = Math.round(d.wind.from / 22.5) * 22.5
+
+					return (
+						<td className='wind' key={i}>
+          					<img src={arrow} className='wind-arrow' alt='N' style={{ transform: 'rotate(' + from_dir + 'deg)' }}/>
+							{speed}
+						</td>
+					)
 				})}
 			</tr>
 		)
