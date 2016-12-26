fefc95289fc1485c506df15d28cacf951c2968e5
tattii <yuta.tatti@gmail.com>
2016-12-27 03:07:47 +0900

    add wind arrow upper

--- src/WindUpperRow.js
+++ src/WindUpperRow.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import arrow from './arrow.svg'
 
 export default class WindUpperRow extends Component {
 	render() {
@@ -9,13 +10,25 @@ export default class WindUpperRow extends Component {
 				{this.props.data.map((d, i) => {
 					let speed = d[level].speed.toFixed(1)
 					if (speed >= 10) speed = Math.round(speed)
+
+					let from_dir = Math.round(d[level].from / 22.5) * 22.5
+
+					let style = {}, colSpan = 3
+
 					if (i === 0){
-						return (<td colSpan={2} style={{ textAlign:'left' }}key={i}>{speed}</td>)
+						colSpan = 2
+						style.textAlign = 'left'
 					}else if (i === length - 1) {
-						return (<td colSpan={2} style={{ textAlign:'Right' }}key={i}>{speed}</td>)
-					}else{
-						return (<td colSpan={3} key={i}>{speed}</td>)
+						colSpan = 2
+						style.textAlign = 'Right'
 					}
+
+					return (
+						<td className='wind-upper' colSpan={colSpan} key={i}>
+							<img src={arrow} className='wind-arrow' alt='N' style={{ transform: 'rotate(' + from_dir + 'deg)' }}/>
+							{speed}
+						</td>
+					)
 				})}
 			</tr>
 		)
