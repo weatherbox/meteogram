29a94775afb288f609f85fe809c7fbf69f4dbc39
tattii <yuta.tatti@gmail.com>
2016-12-27 23:55:36 +0900

    empty for no rain

--- src/RainRow.js
+++ src/RainRow.js
@@ -5,7 +5,7 @@ export default class RainRow extends Component {
 		return (
 			<tr>
 				{this.props.data.map((d, i) => {
-					let rain = (d.rain !== null) ? d.rain.toFixed(1) : '-'
+					let rain = (d.rain !== null && d.rain !== 0) ? d.rain.toFixed(1) : ''
 					if (rain >= 10) rain = Math.round(rain)
 					return (<td key={i}>{rain}</td>)
 				})}
